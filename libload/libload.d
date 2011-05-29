b975c02f7558bb3e666b43be0a5aabd14558c519
DannyArends <Danny.Arends@gmail.com>
2011-05-30 00:43:17 +0200

    Minor updates to lib-load making it as pure as possible

--- libload/libload.d
+++ libload/libload.d
@@ -18,16 +18,14 @@ private import std.conv;
  */
 private void* getFunctionThroughVoid(HXModule shared_library, string functionname){
 	void* symbol = ExeModule_GetSymbol(shared_library, functionname);
-	if (symbol is null) {
-		writeln("Failed to load function address " ~ functionname);
-	}
+	if (symbol is null) throw new Exception("Failed to load function address " ~ functionname);
 	return symbol;
 }
 
 /*
  * Loads a single shared library (dll, so, dylib)
  */
-HXModule load_library(string library_prefix){
+private HXModule load_library(string library_prefix){
   HXModule shared_library = null;
 	version (Windows) {
 		shared_library = ExeModule_Load(library_prefix ~ ".dll");
@@ -36,19 +34,15 @@ HXModule load_library(string library_prefix){
 	} else version (darwin) {
 		shared_library = ExeModule_Load("/usr/lib/"~ library_prefix ~".dylib");
 	}
-  if(shared_library is null){
-    writeln("Unable to find shared library: " ~ library_prefix);
-    return null;
-  }else{
-    debug writeln("Loaded shared library: " ~ library_prefix);
-    return shared_library;
-  }
+  if(shared_library is null) throw new Exception("Unable to find shared library: " ~ library_prefix);
+  debug writeln("Loaded shared library: " ~ library_prefix);
+  return shared_library;
 }
 
 /*
  * Loads a single function (Opens the library_prefix file)
  */
-T load_function(T)(string library_prefix, string functionname) {
+pure T load_function(T)(string library_prefix, string functionname) {
   HXModule shared_library = load_library(library_prefix);
   T returnfunction = load_function!T(shared_library, functionname);
   debug writeln(functionname ~" function mapped from " ~ library_prefix);
@@ -58,6 +52,6 @@ T load_function(T)(string library_prefix, string functionname) {
 /*
  * Loads a single function (Needs a live reference to the library)
  */
-T load_function(T)(HXModule shared_library, string functionname) {
+pure T load_function(T)(HXModule shared_library, string functionname) {
   return (cast(T) getFunctionThroughVoid(shared_library, functionname));
 }
