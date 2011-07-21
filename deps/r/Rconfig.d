e772a784bd4537fefa4f99b11449d1a6fb7a4a71
DannyArends <Danny.Arends@Gmail.com>
2011-07-21 22:58:59 +0200

    Removed unused R binding stuff generated by htod

--- deps/r/Rconfig.d
+++
@@ -1,9 +0,0 @@
-/* Converted to D from Rconfig.h by htod */
-module r.Rconfig;
-
-const HAVE_F77_UNDERSCORE = 1;
-
-const IEEE_754 = 1;
-const SUPPORT_UTF8 = 1;
-const SUPPORT_MBCS = 1;
-const ENABLE_NLS = 1;
--- deps/r/r.d
+++ deps/r/r.d
@@ -6,18 +6,6 @@
  *
  * Copyright (c) 2010 Danny Arends
  *
- *     This program is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU General Public License,
- *     version 3, as published by the Free Software Foundation.
- * 
- *     This program is distributed in the hope that it will be useful,
- *     but without any warranty; without even the implied warranty of
- *     merchantability or fitness for a particular purpose.  See the GNU
- *     General Public License, version 3, for more details.
- * 
- *     A copy of the GNU General Public License, version 3, is available
- *     at http://www.r-project.org/Licenses/GPL-3
- *
  * Contains: 
  * - private: static this
  *
@@ -36,51 +24,19 @@ import core.libload.libload;
 import std.c.stdlib;
 import std.c.stdio;
 import std.c.math;
-import r.Rconfig;
-import r.r_ext.Arith;
-import r.r_ext.Boolean;
-import r.r_ext.Complex;
-import r.r_ext.Constants;
-import r.r_ext.Error;
-import r.r_ext.Memory;
-import r.r_ext.Print;
-import r.r_ext.Random;
-import r.r_ext.Utils;
-import r.r_ext.RS;
 
-//Additional enum defined in R_internals.h
-enum  SEXPTYPE : uint {
-  NILSXP	= 0,	/* nil = NULL */
-  SYMSXP	= 1,	/* symbols */
-  LISTSXP	= 2,	/* lists of dotted pairs */
-  CLOSXP	= 3,	/* closures */
-  ENVSXP	= 4,	/* environments */
-  PROMSXP	= 5,	/* promises: [un]evaluated closure arguments */
-  LANGSXP	= 6,	/* language constructs (special lists) */
-  SPECIALSXP	= 7,	/* special forms */
-  BUILTINSXP	= 8,	/* builtin non-special forms */
-  CHARSXP	= 9,	/* "scalar" string type (internal only)*/
-  LGLSXP	= 10,	/* logical vectors */
-  INTSXP	= 13,	/* integer vectors */
-  REALSXP	= 14,	/* real variables */
-  CPLXSXP	= 15,	/* complex variables */
-  STRSXP	= 16,	/* string vectors */
-  DOTSXP	= 17,	/* dot-dot-dot object */
-  ANYSXP	= 18,	/* make "any" args work */
-  VECSXP	= 19,	/* generic vectors */
-  EXPRSXP	= 20,	/* expressions vectors */
-  BCODESXP	= 21,	/* byte code */
-  EXTPTRSXP	= 22,	/* external pointer */
-  WEAKREFSXP	= 23,	/* weak reference */
-  RAWSXP	= 24,	/* raw bytes */
-  S4SXP	= 25,	/* S4 non-vector */
-  FUNSXP	= 99	/* Closure or Builtin */
-};
+const HAVE_F77_UNDERSCORE = 1;
 
-extern (C):
+const IEEE_754 = 1;
+const SUPPORT_UTF8 = 1;
+const SUPPORT_MBCS = 1;
+const ENABLE_NLS = 1;
+
+extern(C){
   //These are the functions we map to D from Rmath.h
   double function(double, double, double, int) dnorm;
   double function(double, double, double, int, int) qf;
+}
 
 //Load the functions when the module is loaded
 static this(){
