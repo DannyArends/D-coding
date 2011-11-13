7ea28df18defcd14b0411b14bed1e63b49ff4fa3
DannyArends <Danny.Arends@gmail.com>
2011-11-13 21:45:07 +0100

    nicer output

--- src/startJVM.d
+++ src/startJVM.d
@@ -8,7 +8,7 @@ import jni.jni_types;
 import jni.jni_structs;
 
 JNIEnv* create_vm(){
-  writeln("Create VM");
+  writeln("Start creating the Java VM");
 	JavaVM* jvm;
 	JNIEnv* env;
 	JavaVMInitArgs args;
@@ -20,9 +20,8 @@ JNIEnv* create_vm(){
 	args.options = options.ptr;
 	args.ignoreUnrecognized = JNI_FALSE;
   
-  writeln("Before JNI_CreateJavaVM");
 	JNI_CreateJavaVM(&jvm, cast(void**)&env, &args);
-  writeln("After JNI_CreateJavaVM");
+  writeln("Java VM created: " ~ to!string(env));
 	return env;
 }
 
@@ -32,21 +31,21 @@ void invoke_class(JNIEnv* env) {
 	jobjectArray applicationArgs;
 	jstring applicationArg0;
 
-  writeln("Finding the Hellow World class");
+  writeln("Finding InvocationHelloWorld class");
 	helloWorldClass = (*env).functions.FindClass(env, "example/jni/InvocationHelloWorld\0".dup.ptr);
-  writeln("Finding the main method of " ~ to!string(helloWorldClass));
+  writeln("Finding main method: " ~ to!string(helloWorldClass));
 	mainMethod = (*env).functions.GetStaticMethodID(env, helloWorldClass, "main\0".dup.ptr, "([Ljava/lang/String;)V\0".dup.ptr);
-  writeln("Going to execute");
+  writeln("Going to execute: " ~ to!string(mainMethod));
 	applicationArgs = (*env).functions.NewObjectArray(env, 1, (*env).functions.FindClass(env, "java/lang/String\0".dup.ptr), null);
 	applicationArg0 = (*env).functions.NewStringUTF(env, "From-C-program\0".dup.ptr);
+  
 	(*env).functions.SetObjectArrayElement(env, applicationArgs, 0, cast(_jobject*)applicationArg0);
-
 	(*env).functions.CallStaticVoidMethod(env, helloWorldClass, mainMethod, applicationArgs);
-  writeln("Done");
+  writeln("Done with java execution");
 }
 
 
 void main(string[] args){
 	JNIEnv* env = create_vm();
-	invoke_class( env );
+	invoke_class(env);
 }
