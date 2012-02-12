8556b85b2605f8cf8b9e95084fc46f31ea59d820
DannyArends <Danny.Arends@gmail.com>
2012-02-12 19:49:08 +0100

    Some headers

--- src/gui/screen.d
+++ src/gui/screen.d
@@ -1,3 +1,11 @@
+/**********************************************************************
+ * \file src/gui/screen.d
+ *
+ * copyright (c) 2012 Danny Arends
+ * last modified Feb, 2012
+ * first written Dec, 2011
+ * Written in the D Programming Language (http://www.digitalmars.com/d)
+ **********************************************************************/
 module gui.screen;
 
 import std.array;
