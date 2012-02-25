204f459c372529b701ee7cd9983d203926a6decd
DannyArends <Danny.Arends@gmail.com>
2012-02-25 23:43:51 +0100

    Add property to server

--- src/web/server.d
+++ src/web/server.d
@@ -13,83 +13,83 @@ import core.typedefs.types;
 
 class Server(Client) : core.thread.Thread{
   private:
-  Socket serverSocket;
-  SocketSet set;
-  string hostname;
-  ushort port;
-  Client[] clients;
-  uint max_clients;
-  ubyte[1024] buffer;
-  bool verbose = true;
+    Socket       socket;
+    SocketSet    set;
+    string       _hostname    = "0.0.0.0";
+    ushort       _port        = 3000;
+    Client[]     clients;
+    uint         _max_clients = 2000;
+    ubyte[1024]  buffer;
   
   public:
-  
-  this(string hostname = "0.0.0.0", ushort port = 3000, uint max_clients=2000){
-    super(&run);
-    serverSocket = new Socket(AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
-    this.port = port;
-    this.hostname = hostname;
-    this.max_clients = max_clients;
-    with(serverSocket){
-      bind(new InternetAddress(hostname,port));
-      listen(20);
+    this(string hostname = "0.0.0.0", ushort port = 3000, uint max_clients=2000){
+      super(&run);
+      socket = new Socket(AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
+      _port = port;
+      _hostname = hostname;
+      _max_clients = max_clients;
+      with(socket){
+        bind(new InternetAddress(hostname,port));
+        listen(20);
+      }
+      clients = new Client[max_clients];
+      writeln("[Server] Constructed");
     }
-    clients = new Client[max_clients];
-    writeln("Server Constructed");
-  }
+    
+    @property int    max_clients(){ return _max_clients; }
+    @property int    port(){return _port; }
+    @property string hostname(){return _hostname; }
 
-  void run(){
-    runLoop();
-  }
-  
-  void runLoop(string root_path = "."){
-    writeln("Server: Start listening for clients");
-    set = new SocketSet();
-    while(true){
-      assert(set !is null);
-      set.reset();
-      set.add(serverSocket);
-      foreach(Client c; clients){
-        if(c !is null){
-          set.add(c.socket);
+    void run(){
+      writeln("[Server] Start listening for clients");
+      set = new SocketSet();
+      while(true){
+        assert(set !is null);
+        set.reset();
+        set.add(socket);
+        foreach(Client c; clients){
+          if(c !is null){
+            set.add(c.socket);
+          }
         }
-      }
-      int result = Socket.select(set, null, null, 50000);
-      if(result > 0) {
-        if(set.isSet(serverSocket)) {
-          Socket sock = serverSocket.accept();
-          assert(sock !is null);
-          uint index;
-          for(index = 0; index < clients.length; index++) {
-            if(clients[index] is null) {
-              clients[index] = new Client(this, sock, cast(uint)index);
-              clients[index].start();
-              writefln("Server: Accepted connection on %d",index);
-              break;
+        int result = Socket.select(set, null, null, 50000);
+        if(result > 0) {
+          if(set.isSet(socket)) {
+            Socket sock = socket.accept();
+            if(sock !is null){
+              uint index;
+              for(index = 0; index < clients.length; index++) {
+                if(clients[index] is null) {
+                  clients[index] = new Client(this, sock, cast(uint)index);
+                  clients[index].start();
+                  writefln("[Server] Accepted connection on %d",index);
+                  break;
+                }
+              }
+              if(index == clients.length) {
+                sock.close();
+              }
+              if(result == 1) continue;
             }
           }
-          if(index == clients.length) {
-            sock.close();
-          }
-          if(result == 1) continue;
-        }
-        foreach(uint index, ref Client fib; clients) {
-          if(fib is null) continue;
-          if(set.isSet(fib.socket)){
-            auto received = fib.socket.receive(buffer);
-            if(received <= 0) {
-              clients[index].close();
-              clients[index].offline();
-              clients[index] = null;
-              writefln("Server: Dropped connection %d",index);
-              continue;
-            }else{
-              clients[index].processCommand(buffer[0..received]);
-              writefln("Server: Received from %d: %s",index, to!string(toType!char(buffer[0..received])));
+          foreach(uint index, ref Client fib; clients) {
+            if(fib is null) continue;
+            if(set.isSet(fib.socket)){
+              auto received = fib.socket.receive(buffer);
+              if(received <= 0) {
+                clients[index].close();
+                clients[index].offline();
+                clients[index] = null;
+                writefln("[Server] Dropped connection %d",index);
+                continue;
+              }else{
+                clients[index].processCommand(buffer[0..received]);
+                writefln("[Server] Received from %d: %s",index, to!string(toType!char(buffer[0..received])));
+              }
             }
           }
         }
       }
+      writeln("[Server] Shutdown");
     }
-  }
 }
