528f4252444299b7cab46bff685fb9783ed13d06
DannyArends <Danny.Arends@gmail.com>
2012-04-09 23:21:07 +0200

    [deps/r] Header + more functions

--- deps/r/r.d
+++ deps/r/r.d
@@ -1,8 +1,12 @@
-/**********************************************************************
- * \file deps/r/r.d - Wrapper for R
+/******************************************************************//**
+ * \file deps/r/r.d
+ * \brief Wrapper for R statistical programming language
  *
+ * <i>Copyright (c) 2012</i> Danny Arends<br>
+ * Last modified Apr, 2012<br>
+ * First written 2010<br>
  * Written in the D Programming Language (http://www.digitalmars.com/d)
- **********************************************************************/ 
+ **********************************************************************/
 module r.r;
 
 private import std.loader;
@@ -11,23 +15,29 @@ private import std.conv;
 
 import libload.libload;
 
-const HAVE_F77_UNDERSCORE = 1;
-
-const IEEE_754 = 1;
-const SUPPORT_UTF8 = 1;
-const SUPPORT_MBCS = 1;
-const ENABLE_NLS = 1;
-
 extern(C){
-  //These are the functions we map to D from Rmath.h
+  void function(size_t argc, char **argv) Rf_initEmbeddedR;
+  void function(size_t) Rf_endEmbeddedR;
+  void function() R_SaveGlobalEnv;
+  void function() GetRNGstate;
+  void function() PutRNGstate;
+  
   double function(double, double, double, int) dnorm;
   double function(double, double, double, int, int) qf;
 }
 
-//Load the functions when the module is loaded
-static this(){
+static this(){//Load the functions when the module is loaded
   HXModule lib = load_library("R");
+  
+  load_function(GetRNGstate)(lib,"GetRNGstate");
+  load_function(PutRNGstate)(lib,"PutRNGstate");  
+  load_function(Rf_initEmbeddedR)(lib,"Rf_initEmbeddedR");
+  load_function(Rf_endEmbeddedR)(lib,"Rf_endEmbeddedR");
+  load_function(R_SaveGlobalEnv)(lib,"R_SaveGlobalEnv");
+  
   load_function(dnorm)(lib,"Rf_dnorm4");
   load_function(qf)(lib,"Rf_qf");
+
   writeln("Loaded R functionality");
 }
+
